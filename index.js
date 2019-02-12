7906cf10e48c2be794a73a475e0101f288524203
Gilbert Torchon <gilbert@noukod.com>
2019-02-12 11:12:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function createPost(e){
+  e.preventDefault();
+  const author=document.getElementById("postAuthor");
+  const title=document.getElementById("postTitle");
+  const body=document.getElementById("postBody");
+}
